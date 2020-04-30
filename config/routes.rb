796d1595b7c87399535bc84c7876b58c85aebba7
LieuFukunaga <heartarium01208.2@gmail.com>
2020-04-30 10:08:33 +0900

    mount login functiion in wizard format

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  # users/registrationsコントローラのnew_addressアクション、create_addressアクションとして振る舞うということ？
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
+
   root to: "lists#index"
+
   $date = Time.now.to_s
+
 end
