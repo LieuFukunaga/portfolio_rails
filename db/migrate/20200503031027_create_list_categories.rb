6273b69a21d431160ec86edbcd2bd28310714c59
LieuFukunaga <heartarium01208.2@gmail.com>
2020-05-03 12:32:10 +0900

    (before excute migrating) edit: migration files

--- db/migrate/20200503031027_create_list_categories.rb
+++ db/migrate/20200503031027_create_list_categories.rb
@@ -1,8 +1,13 @@
 class CreateListCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :list_categories do |t|
+      t.references :list, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
+    add_index :lists_categories, :list_id
+    add_index :lists_categories, :category_id
+    add_index :lists_categories, [:list_id,:category_id],unique: true
   end
 end
--- db/migrate/20200503031452_add_columns_to_lists.rb
+++ db/migrate/20200503031452_add_columns_to_lists.rb
@@ -1,4 +1,7 @@
 class AddColumnsToLists < ActiveRecord::Migration[5.2]
   def change
+    add_column :lists, :name, :string, null: false
+    add_reference :lists, :user, foreign_key: true
+    add_index :lists, :name, :created_at
   end
 end
