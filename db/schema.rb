8f87decc48fd18b18381b06e86081f5e27a1329e
LieuFukunaga <heartarium01208.2@gmail.com>
2020-05-06 22:01:55 +0900

    mount datetime: goals#new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_010512) do
+ActiveRecord::Schema.define(version: 2020_05_06_123750) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_05_06_010512) do
     t.bigint "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "date"
     t.index ["list_id"], name: "index_goals_on_list_id"
   end
 
