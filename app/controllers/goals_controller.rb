7f15d6bb91fc07e59dbf426ed9a077f41a3c057d
LieuFukunaga <heartarium01208.2@gmail.com>
2020-05-07 08:59:40 +0900

    update: category_function

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -11,9 +11,9 @@ class GoalsController < ApplicationController
 
   def create
     @goal = Goal.new(goal_params)
-    # binding.pry
+    binding.pry
     # 作成されたリストに紐付けられたカテゴリをカンマで分割、配列にして変数に代入
-    inputs = params[:goal][:categories_attributes][:"0"][:"category_name"].split
+    inputs = params[:goal][:categories_attributes][:"0"][:"category_name"].split(/[ 　,、]/)
     # respond_to do |format|
       if @goal.save!
         @goal.save_category(inputs)
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   has_many :goal_categories, dependent: :destroy
   has_many :goals, through: :goal_categories
 
-  validates :category_name, presence:true, length:{maximum:50}
+  validates :category_name, presence:true
   validates :category_name, null: false, null: false
 
   accepts_nested_attributes_for :goals
