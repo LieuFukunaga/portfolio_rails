fcb2c68d68682b20f4641b55232389e7af24f807
LieuFukunaga <heartarium01208.2@gmail.com>
2020-05-08 20:04:05 +0900

    update: categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,38 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update]
+  before_action :set_categories, only: [:index]
+
   def index
-    @categories = Category.includes(:goals)
   end
 
   def show
-    @category = Category.find(params[:id])
     @goals = @category.goals
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path
+      flash[:success] = "カテゴリを更新しました"
+    else
+      flash.now[:alert] = @category.errors.full_messages
+      render action: :edit
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:category_name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def set_categories
+    @categories = Category.includes(:goals)
   end
 end
