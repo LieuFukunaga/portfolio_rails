052136ccb6370cdf88e0a0eb5514e90c1359259b
LieuFukunaga <heartarium01208.2@gmail.com>
2020-05-12 13:48:49 +0900

    update: personaly category function

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   def index
-    @categories = Category.includes(:goals)
+    @categories = current_user.categories
   end
 
   def show
