fe4223a011a4144c74077855b77fccd7c94339fb
LieuFukunaga <heartarium01208.2@gmail.com>
2020-05-12 10:35:17 +0900

    change: categories_index.js

--- app/assets/javascripts/categories_index.js
+++ app/assets/javascripts/categories_index.js
@@ -1,10 +1,10 @@
-// $(function(){
-//   $(".categories-index__delete-btn").click(function(){
-//     let parent = $(this).parent();
-//     parent = parent.data('index');
-//     let categoryName = $(`#category-name_${parent}`).text();
-//     if (!confirm(`${categoryName}を削除してよろしいですか？`)){
-//       return false;
-//     };
-//   });
-// });
+$(function(){
+  $(document).on("click", ".categories-index__delete-btn", function(){
+    let parent = $(this).parent();
+    parent = parent.data('index');
+    let categoryName = $(`#category-name_${parent}`).text();
+    if (!confirm(`${categoryName}を削除してよろしいですか？`)){
+      return false;
+    };
+  });
+});
