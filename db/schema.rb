9170d19f7ea6d200d239d820956d8bca7468d5e1
LieuFukunaga <heartarium01208.2@gmail.com>
2020-05-27 21:53:58 +0900

    create step

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_111258) do
+ActiveRecord::Schema.define(version: 2020_05_27_125259) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -82,6 +82,11 @@ ActiveRecord::Schema.define(version: 2020_05_11_111258) do
     t.bigint "user_id"
     t.index ["list_name"], name: "index_lists_on_list_name"
     t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "steps", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
