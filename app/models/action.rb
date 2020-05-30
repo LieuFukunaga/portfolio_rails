9d374904ce6e4d7c4ebe822444469ea934d9920d
LieuFukunaga <heartarium01208.2@gmail.com>
2020-05-30 14:50:59 +0900

    set associations for Step and Action

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,2 +1,17 @@
 class Action < ApplicationRecord
+  belongs_to :user
+  belongs_to :list
+  belongs_to :goal
+  belongs_to :step
+
+  validates :title, format: {with:/\A[^[:blank:]\s、,]+\z/}
+  validates :title, length: { minimum: 0, maximum: 45 }
+  validates :title, null: false, presence: true
+
+  has_one_attached :image
+
+  enum status: {
+    doing: 0,
+    done: 1,
+  }
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,2 +1,18 @@
 class Step < ApplicationRecord
+  belongs_to :user
+  belongs_to :list
+  belongs_to :goal
+
+  validates :title, format: {with:/\A[^[:blank:]\s、,]+\z/}
+  validates :title, length: { minimum: 0, maximum: 45 }
+  validates :title, null: false, presence: true
+
+  has_many :actions
+
+  has_one_attached :image
+
+  enum status: {
+    doing: 0,
+    done: 1,
+  }
 end
